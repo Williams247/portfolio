b9a8075ab758304a187b9ebfe4d1de7094d7be01
Williams Alex <godswillwilliams9@gmail.com>
2024-02-19 00:44:34 +0100

    Fix: Fixed Contact Form for portfolio website

--- src/components/contact-form/index.tsx
+++ src/components/contact-form/index.tsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
-import { Formik } from "formik";
+import { useFormik } from "formik";
 import { TextInput, TextArea } from "../input";
 import { Button } from "../button";
-import { contactValidation, PayloadProps, contactConstant } from "./utils";
+import { contactFormSchema, PayloadProps, contactConstant } from "./utils";
 
 export const ContactForm = () => {
   const [loading, setLoading] = useState(false);
+
+  const formik = useFormik({
+    initialValues: contactConstant,
+    validationSchema: contactFormSchema,
+    onSubmit: (payload) => handleSendMessage(payload),
+  });
+
   const handleSendMessage = async (payload: PayloadProps) => {
     setLoading(true);
+
     try {
       const response = await fetch(
         `${process.env.REACT_APP_PUBLIC_BASE_URL}/V2/info/send-message`,
@@ -18,81 +26,73 @@ export const ContactForm = () => {
           body: JSON.stringify(payload),
         }
       );
+
       const data = await response.json();
-      console.log(data.message);
       setLoading(false);
+
+      formik.resetForm();
       toast.success(data.message, { position: "top-right" });
     } catch (error) {
-      console.log(error);
       setLoading(false);
     }
   };
+
   return (
     <div id={"message"} className={"mt-14 py-6 md:px-5 lg:px-8"}>
       <div>
         <div>
-          <Formik
-            validationSchema={contactValidation}
-            validateOnChange
-            validateOnBlur
-            initialValues={contactConstant}
-            onSubmit={handleSendMessage}
-          >
-            {({ values, errors, handleChange, handleSubmit, touched }) => (
-              <form onSubmit={handleSubmit}>
-                <div>
-                  <div>
-                    <TextInput
-                      label="Email"
-                      isRequired
-                      htmlFor={"email"}
-                      name={"email"}
-                      changeEvent={handleChange}
-                      inputType="email"
-                      errors={errors}
-                      touched={touched}
-                      inputValue={values.email}
-                      placeHolder="Email"
-                    />
-                  </div>
-                  <div>
-                    <TextInput
-                      label="From Who"
-                      isRequired
-                      htmlFor={"from"}
-                      name={"from"}
-                      changeEvent={handleChange}
-                      inputType="text"
-                      errors={errors}
-                      touched={touched}
-                      inputValue={values.from}
-                      placeHolder="From who"
-                    />
-                  </div>
-                </div>
-                <div className="mt-1">
-                  <TextArea
-                    label="Message"
-                    isRequired
-                    htmlFor={"message"}
-                    name={"message"}
-                    changeEvent={handleChange}
-                    errors={errors}
-                    touched={touched}
-                    inputValue={values.message}
-                    placeHolder={"Leave a message"}
-                  />
-                </div>
-                <div className="mt-6">
-                  <Button
-                    disabled={loading}
-                    label={`${loading ? "Loading...." : "Send message"}`}
-                    variant={"primary"}
-                  />
-                </div>
-              </form>
-            )}
-          </Formik>
+          <form onSubmit={formik.handleSubmit}>
+            <div>
+              <div>
+                <TextInput
+                  label={"Email"}
+                  isRequired
+                  htmlFor={"email"}
+                  name={"email"}
+                  changeEvent={formik.handleChange}
+                  inputType="email"
+                  errors={formik.errors}
+                  touched={formik.touched}
+                  inputValue={formik.values.email}
+                  placeHolder={"Email"}
+                />
+              </div>
+              <div>
+                <TextInput
+                  label={"From Who"}
+                  isRequired
+                  htmlFor={"from"}
+                  name={"from"}
+                  changeEvent={formik.handleChange}
+                  inputType="text"
+                  errors={formik.errors}
+                  touched={formik.touched}
+                  inputValue={formik.values.from}
+                  placeHolder={"From who"}
+                />
+              </div>
+            </div>
+            <div className="mt-1">
+              <TextArea
+                label="Message"
+                isRequired
+                htmlFor={"message"}
+                name={"message"}
+                changeEvent={formik.handleChange}
+                errors={formik.errors}
+                touched={formik.touched}
+                inputValue={formik.values.message}
+                placeHolder={"Leave a message"}
+              />
+            </div>
+            <div className="mt-6">
+              <Button
+                disabled={loading}
+                label={`${loading ? "Loading...." : "Send message"}`}
+                variant={"primary"}
+              />
+            </div>
+          </form>
         </div>
       </div>
     </div>
--- src/components/contact-form/utils.ts
+++ src/components/contact-form/utils.ts
@@ -1,6 +1,6 @@
 import * as yup from "yup";
 
-export const contactValidation = yup.object().shape({
+export const contactFormSchema = yup.object().shape({
   email: yup.string().email().required("email is required"),
   from: yup.string().required("your message credential is required"),
   message: yup.string().required("your message field is required"),
